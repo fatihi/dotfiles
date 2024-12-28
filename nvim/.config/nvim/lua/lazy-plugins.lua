43c4f840d77810c623bf629331b1567686bba6df
Fatih Inan <fatihinan@gmail.com>
2024-12-28 19:52:43 +0100

    Move plugins to plugins folder

--- nvim/.config/nvim/lua/lazy-plugins.lua
+++ nvim/.config/nvim/lua/lazy-plugins.lua
@@ -4,37 +4,7 @@ require('lazy').setup({
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
-  require 'kickstart.plugins.gitsigns',
-
-  require 'kickstart.plugins.which-key',
-
-  require 'kickstart.plugins.telescope',
-
-  require 'kickstart.plugins.lspconfig',
-
-  require 'kickstart.plugins.conform',
-
-  require 'kickstart.plugins.cmp',
-
-  require 'kickstart.plugins.tokyonight',
-
-  require 'kickstart.plugins.todo-comments',
-
-  require 'kickstart.plugins.mini',
-
-  require 'kickstart.plugins.treesitter',
-
-  -- require 'kickstart.plugins.debug',
-  --
-  -- require 'kickstart.plugins.indent_line',
-  --
-  -- require 'kickstart.plugins.lint',
-  --
-  -- require 'kickstart.plugins.autopairs',
-  --
-  -- require 'kickstart.plugins.neo-tree',
-
-  { import = 'custom.plugins' },
+  { import = 'plugins' },
 }, {
   ui = {
     icons = vim.g.have_nerd_font and {} or {
