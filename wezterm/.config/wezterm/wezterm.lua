e73d19bd974b57952d24ec42d2fa20d7e1a423b6
Fatih Inan <fatihinan@gmail.com>
2025-02-21 21:30:38 +0100

    Add config check to wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,13 @@
 local wezterm = require("wezterm")
 
-local config = wezterm.config_builder()
+local config = {}
 
+-- Use config builder if possible
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- Settings
 config.color_scheme = "tokyonight_night"
 config.font = wezterm.font("FiraCode Nerd Font")
 
