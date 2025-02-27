3fa6adfae90dfd0d1b059a0204b342cd68c1091d
Fatih Inan <fatihinan@gmail.com>
2025-02-27 21:33:48 +0100

    Show hidden files in oil

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -4,7 +4,12 @@ return {
     { 'echasnovski/mini.icons', opts = {} },
   },
   config = function()
-    require('oil').setup()
+    require('oil').setup {
+      default_file_explorer = true,
+      view_options = {
+        show_hidden = true,
+      },
+    }
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
   end,
 }
