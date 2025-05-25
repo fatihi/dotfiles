a207b483d2af4b0adc913bcd7ddd01b5a066f970
Fatih Inan <fatihinan@gmail.com>
2025-05-25 12:55:39 +0200

    Disable wayland in wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,9 +1,14 @@
-local wezterm = require "wezterm"
+local wezterm = require("wezterm")
 local act = wezterm.action
 local config = {}
 
 -- Use config builder if possible
-if wezterm.config_builder() then config = wezterm.config_builder() end
+if wezterm.config_builder() then
+	config = wezterm.config_builder()
+end
+
+-- Disable wayland
+config.enable_wayland = false
 
 -- Settings
 config.color_scheme = "nordfox"
@@ -23,28 +28,28 @@ config.default_workspace = "main"
 -- Dim inactive panes
 config.inactive_pane_hsb = {
 	saturation = 0.25,
-	brightness = 0.5
+	brightness = 0.5,
 }
 
 -- Keybinds
-config.leader = { key = "a", mods="CTRL", timeout_milliseconds = 1000 }
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
 	-- Send C-a when pressing C-a twice
-	{ key = "a", mods = "LEADER", action = act.SendKey { key = "a", mods = "CTRL" } },
+	{ key = "a", mods = "LEADER", action = act.SendKey({ key = "a", mods = "CTRL" }) },
 	{ key = "c", mods = "LEADER", action = act.ActivateCopyMode },
 
 	-- Panes
-	{ key = "-", mods = "LEADER", action = act.SplitVertical { domain = "CurrentPaneDomain" } },
-	{ key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
+	{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
 	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
 	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
 	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
-	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane { confirm = true } },
+	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
 	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
 	{ key = "s", mods = "LEADER", action = act.RotatePanes("Clockwise") },
 	-- KeyTable for resizing panes
-	{ key = "r", mods = "LEADER", action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
+	{ key = "r", mods = "LEADER", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
 
 	-- Tabs
 	{ key = "n", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
@@ -52,10 +57,10 @@ config.keys = {
 	{ key = "]", mods = "LEADER", action = act.ActivateTabRelative(1) },
 	{ key = "t", mods = "LEADER", action = act.ShowTabNavigator },
 	-- KeyTable for moving tabs around
-	{ key = "m", mods = "LEADER", action = act.ActivateKeyTable { name = "move_tab", one_shot = false } },
+	{ key = "m", mods = "LEADER", action = act.ActivateKeyTable({ name = "move_tab", one_shot = false }) },
 
 	-- Workspaces
-	{ key = "w", mods = "LEADER", action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
+	{ key = "w", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
 }
 
 -- Navigate tabs with index
@@ -69,10 +74,10 @@ end
 
 config.key_tables = {
 	resize_pane = {
-		{ key = "h", action = act.AdjustPaneSize { "Left", 1 } },
-		{ key = "j", action = act.AdjustPaneSize { "Down", 1 } },
-		{ key = "k", action = act.AdjustPaneSize { "Up", 1 } },
-		{ key = "l", action = act.AdjustPaneSize { "Right", 1 } },
+		{ key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
 		{ key = "Escape", action = "PopKeyTable" },
 		{ key = "Enter", action = "PopKeyTable" },
 	},
@@ -83,21 +88,21 @@ config.key_tables = {
 		{ key = "l", action = act.MoveTabRelative(1) },
 		{ key = "Escape", action = "PopKeyTable" },
 		{ key = "Enter", action = "PopKeyTable" },
-	}
+	},
 }
 
 -- Tab bar
 config.use_fancy_tab_bar = false
 config.status_update_interval = 1000
-wezterm.on("update-status", function (window, pane)
-	local shorten_name = function (s)
+wezterm.on("update-status", function(window, pane)
+	local shorten_name = function(s)
 		return string.gsub(s, "(.*[/\\])(.*)", "%2")
 	end
 
 	-- Workspace name
 	local stat = window:active_workspace()
 	local stat_color = custom_colors.red
-	
+
 	if window:active_key_table() then
 		stat = window.active_key_table()
 		stat_color = custom_colors.cyan
